3c167499ad94e0bd5b43bd923f3c18b0126033aa
DevWithTheDan <67156578+DevWithTheDan@users.noreply.github.com>
2023-11-17 17:03:56 +0000

    update comment

--- RoadTripDb/Repos/IBaseRepo.cs
+++ RoadTripDb/Repos/IBaseRepo.cs
@@ -3,37 +3,37 @@
     public interface IBaseRepo<T>
     {
         /// <summary>
-        /// Add a single entity
+        /// Add a single entity of <typeparamref name="T"/>
         /// </summary>
         /// <param name="entity"></param>
         /// <returns></returns>
         Task<T> AddAsync(T entity);
         /// <summary>
-        /// Update a single entity
+        /// Update a single entity of <typeparamref name="T"/>
         /// </summary>
         /// <param name="entity"></param>
         /// <returns></returns>
         Task<T> UpdateAsync(T entity);
         /// <summary>
-        /// Delete a single entity
+        /// Delete a single entity of <typeparamref name="T"/>
         /// </summary>
         /// <param name="entity"></param>
         /// <returns></returns>
         Task RemoveAsync(T entity);
         /// <summary>
-        /// Add a range of entities
+        /// Add a range of entities of <typeparamref name="T"/>
         /// </summary>
         /// <param name="entities"></param>
         /// <returns></returns>
         Task<ICollection<T>> AddRangeAsync(ICollection<T> entities);
         /// <summary>
-        /// Update a range of entities
+        /// Update a range of entities of <typeparamref name="T"/>
         /// </summary>
         /// <param name="entities"></param>
         /// <returns></returns>
         Task<ICollection<T>> UpdateRangeAsync(ICollection<T> entities);
         /// <summary>
-        /// Remove a ranage of entities
+        /// Remove a ranage of entities of <typeparamref name="T"/>
         /// </summary>
         /// <param name="entities"></param>
         /// <returns></returns>
