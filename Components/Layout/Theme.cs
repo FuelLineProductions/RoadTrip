bac7b519c6a376f8e569f82d9d25d9feae1d637b
DevWithTheDan <67156578+DevWithTheDan@users.noreply.github.com>
2023-11-18 17:24:18 +0000

    better ui

--- Components/Layout/Theme.cs
+++ Components/Layout/Theme.cs
@@ -20,8 +20,8 @@ namespace RoadTrip.Components.Layout
                 },
                 PaletteDark = new PaletteDark()
                 {
-                    Surface= "#2f3631",
-                    Primary= "#9c2308"
+                    Surface = "#2f3631",
+                    Primary = "#9c2308"
                 }
 
             };
